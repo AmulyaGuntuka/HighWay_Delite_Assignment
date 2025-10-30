13d951b11a60afe7419387ff7082b469b14e2094
Amulya Guntuka <157285275+AmulyaGuntuka@users.noreply.github.com>
2025-10-30 19:09:56 +0530

    Update api.ts

--- bookit/frontend/src/api/api.ts
+++ bookit/frontend/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL || "https://highway-delite-backend-048l.onrender.com/api",
   headers: { 'Content-Type': 'application/json' },
 });
 
